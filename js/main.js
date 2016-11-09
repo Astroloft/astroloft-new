77359ccd767c26fff07e2842833998f196668995
Louis Morgan <louismorgandesign@gmail.com>
2016-11-09 17:00:05 +0000

    jquery gallery problems

--- js/main.js
+++ js/main.js
@@ -2,3 +2,26 @@ $('#toggle').click(function() {
    $(this).toggleClass('active');
    $('#overlay').toggleClass('open');
   });
+
+
+
+
+$(document).ready(function(){
+  $('.category_item').click(function(){
+    var category = $(this).attr('id');
+
+    if(category == 'all'){
+      $('.filter_item').addClass('hide');
+      setTimeout(function(){
+        $('.filter_item').removeClass('hide');
+      }, 300);
+    } else {
+      $('.filter_item').addClass('hide');
+      setTimeout(function(){
+        $('.' + category).removeClass('hide');
+      }, 300);
+    }
+
+    // prevent linking behaviour
+  });
+});
